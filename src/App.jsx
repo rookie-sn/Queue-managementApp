e1f92a75fce4afde711b451d51599f31fde1a039
rookie-sn <kavinesh342003@gmail.com>
2026-06-28 21:37:42 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+      <h1>Queue Management Application</h1>
+      <p>Manage your customers efficiently</p>
+      </header>
+    </div>
+  );
+}
